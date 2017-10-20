6c467e621ade31c5b5c883c715c03bf1494cf372
summit1219 <291019134@qq.com>
2017-10-21 00:33:00 +0800

    最新

--- determinant/determinant/main.cpp
+++ determinant/determinant/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include<string>
+#include<math.h>
 
 using namespace std;
 
-//void Determinant_Multiplication(Determinant a, Determinant b);
+
 
 struct Determinant {
-	int matrix[10][10];
+	int matrix[2][2];
 };
 
 void Determinant_Multiplication(Determinant a, Determinant b) {
@@ -16,20 +17,27 @@ void Determinant_Multiplication(Determinant a, Determinant b) {
 	int b_row = sizeof(b.matrix) / sizeof(b.matrix[0]);
 	int b_column = sizeof(b.matrix[0]) / sizeof(b.matrix[0][0]);
 
-	for(int i=0;i<a_row;i++)
+	for (int i = 0; i<a_row; i++)
 		for (int j = 0; j < b_column; j++) {
 			int sum = 0;
 			for (int k = 0; k < a_column; k++) {
-				sum = +a.matrix[i][k] * b.matrix[k][j];
+				sum = sum + a.matrix[i][k] * b.matrix[k][j];
 			}
 			c.matrix[i][j] = sum;
 		}
-			
-		
+	for (int i = 0; i < a_row; i++) {
+		for (int j = 0; j < b_row; j++)
+			cout << c.matrix[i][j] << " ";
+		cout << endl;
+	}
+
+	//cout << c.matrix[0][1];
+
+
 
 }
 
-int reverse(int sequence[],int size) {
+int reverse(int sequence[], int size) {
 	int result = 0;
 
 	for (int index1 = 0; index1 < size; index1++) {
@@ -43,35 +51,34 @@ int reverse(int sequence[],int size) {
 }
 
 
-void GetPermutation(int size,int c[10][10]) {
+int GetPermutation(int size, int sequence[2][2]) {
 	int count = 0;
-	int max;                   //用来存放最大的可移动数字
+	int max;
 	int sum = 1;
-	int a[10],b[10];   //a数组用来存放数字，b数组用来存放对应数字的方向
+	int a[2], b[2];
 
 	for (int i = 1; i <= size; i++) {
-		
-		sum = sum*i;                   //根据数字的个数算出全排列的总数
+
+		sum = sum*i;
 
 	}
 
 	for (int i = 0; i < size; i++) {
-		a[i] = i+1;
-		b[i] = 0;                        //0代表向左，1代表向右，一开始所有数字方向默认向左
+
+		b[i] = 0;
+		a[i] = i + 1;
 	}
 
-	while (count<sum) {                                      //当计数未达到全排列数时，继续循环
+	while (count<sum) {
 		int j = 0;
-		int index;                                           //用来保存最大可移动数字的数组下标
+		int index;
+		int max_number = size;
 
 		for (int i = 0; i < size; i++) {
-			c[count][i] = a[i];
+			sequence[count][i] = a[i];
 		}
-
-
-		for (int i = 0; i < size; i++) {                     //找到最大的可移动数字
-			int j = size;
-			if (a[i] == j) {
+		for (int i = 0; i < size; i++) {
+			if (a[i] == max_number) {
 				if ((b[i] == 1 && i != (size - 1) && a[i] > a[i + 1]) || (b[i] == 0 && i != 0 && a[i] > a[i - 1])) {
 					max = a[i];
 					index = i;
@@ -79,12 +86,11 @@ void GetPermutation(int size,int c[10][10]) {
 				}
 				else
 					i = -1;
-				j--;
+				max_number--;
 			}
-
 		}
 
-		if (b[index] == 0) {                  //根据方向交换数字
+		if (b[index] == 0) {
 
 			a[index] = a[index - 1];
 			a[index - 1] = max;
@@ -102,23 +108,69 @@ void GetPermutation(int size,int c[10][10]) {
 
 		}
 
-		for (int i = 0; i < size; i++) {       //改变比移动数字大的数的方向
+		for (int i = 0; i < size; i++) {
 			if (a[i] > max)
 				if (b[i] == 1)
 					b[i] = 0;
 				else if (b[i] == 0)
 					b[i] = 1;
 		}
+		count++;
+	}
+	return count;
+}
 
+int determinat_value(Determinant a) {
+	int result = 0;
+	int number = sizeof(a.matrix[0]) / sizeof(a.matrix[0][0]);
+	int sort[2][2], b[5];
+	int count = GetPermutation(number, sort);
+	for (int i = 0; i < count; i++) {
+		int result1 = 1;
+		for (int j = 0; j < number; j++) {
+			b[j] = sort[i][j];
+		}
+		for (int k = 0; k<number; k++)
+			result1 = result1*a.matrix[k][b[k] - 1];
 
-		
-		count++;                                //每列出一个排序后计数加一
-
+		result = result + pow(-1, reverse(b, number))*result1;
 	}
 
+	return result;
 }
 
 
-void main() {
-	cout << "Thisi is a3" << endl;
+int main() {
+	Determinant a = { 1,2,3,4 };
+	Determinant b = { 1,2,3,4 };
+
+	cout << "行列式a：" << endl;
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++)
+			cout << a.matrix[i][j] << ' ';
+		cout << endl;
+	}
+	cout << endl;
+
+	cout << "行列式b：" << endl;
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++)
+			cout << b.matrix[i][j] << ' ';
+		cout << endl;
+	}
+	cout << endl;
+
+	cout << "行列式a,b相乘后的结果为：" << endl;
+	Determinant_Multiplication(a, b);
+
+	int c[2][2];
+	int d = GetPermutation(2, c);
+	cout << endl;
+
+	cout << "行列式a的值为：" << determinat_value(a);
+
+
+
+
+	return 0;
 }
